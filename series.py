a3ef935d8b11ca5ec4f3e09e99b297298357cf24
EvyHaan <evy.haan@gmail.com>
2019-02-19 20:58:45 -0800

    refactor because this challenge assumes fibonacci sequence starts 0, 1 not 1, 1

--- series.py
+++ series.py
@@ -7,12 +7,12 @@ def fibonacci(n):
     """
     Overview of fibonacci
     """
-    prev = 1
+    prev = 0
     current_fib = 1
-    if n <= 2:
-        print(current_fib)
+    if n == (prev + 1):
+        return(prev)
     else:
-        for i in range(n-2):
+        for i in range(n - current_fib):
             prev, current_fib = current_fib, (prev + current_fib)
     return(current_fib)
 
@@ -24,12 +24,18 @@ def lucas(n):
     prev = 2
     current_lucas = 1
 
-    if n < 2:
+    if (n + 1) == prev:
         return(prev)
-    elif n == 2:
-        return(current_lucas)
-    elif n > 2:
-        for i in range(n-2):
+    else:
+        for i in range(n - prev):
             prev, current_lucas = current_lucas, (prev + current_lucas)
 
     return(current_lucas)
+
+
+# def sum_series(n, option1=0, option2=1):
+#     """
+#     overview of sum_series
+#     """
+#     
+#     return(current_val)
--- test_series.py
+++ test_series.py
@@ -9,13 +9,13 @@ def test_valid_n_fibonacci():
 
 def test_less_than_two_fibonacci():
     actual = fibonacci(1)
-    expected = 1
+    expected = 0
     assert expected == actual
 
 
 # FIBONACCI TESTS
 # 1 test a working instance
-# 2 test an instance of n=0
+# 2 test an instance of n=1
 
 def test_valid_n_lucas():
     actual = lucas(8)
@@ -35,10 +35,24 @@ def test_n_is_two_lucas():
     assert expected == actual
 
 # LUCAS TESTS
-# 1 test a working instance
+# 1 test a working instance > 2
 # 2 test an instance of n=1
 # 3 test an instance of n=2
 
 
+# def test_requred_parameters_sum():
+#     actual = sum_series(8)
+#     expected = 21
+#     assert expected == actual
+
+
+# def test_requred_parameters_sum():
+#     actual = sum_series(8, 2, 1)
+#     expected = 21
+#     assert expected == actual
 
 # SUM TESTS
+# 1 test with required parameter, fibonacci
+# 2 test with required and optional parameters, fibonacci
+# 3 test with required and optional parameters, lucas
+# 4 test with required parameter and optional parameters, alternate
